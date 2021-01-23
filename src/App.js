e9f8b1cf7946d95073760759f49cd474aa7b6828
hojunin <hjinn@smartauth.kr>
2021-01-23 21:28:17 +0900

    git init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { PaymentPage } from '../src/screens/payment/PaymentPage'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
+          나는 홈페이지를 만들꺼야
+        </p> */}
+        <PaymentPage/>
+        <button>
+          Button
+        </button>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
---
+++ src/screens/payment/PaymentPage.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import jQuery from "jquery";
+import {SearchBar} from 'react-native-elements'
+window.$ = window.jQuery = jQuery
+var IMP = window.IMP; 
+IMP.init("imp10942072"); 
+
+export const PaymentPage = () => {
+  const callPaymentModule = () => {
+    IMP.request_pay({ // param
+      pg: "html5_inicis",
+      pay_method: "card",
+      merchant_uid: "ORD20180131-0000011",
+      name: "노르웨이 회전 의자",
+      amount: 64900,
+      buyer_email: "gildong@gmail.com",
+      buyer_name: "홍길동",
+      buyer_tel: "010-4242-4242",
+      buyer_addr: "서울특별시 강남구 신사동",
+      buyer_postcode: "01181"
+    }, rsp => { // callback
+      if (rsp.success) {
+          console.log("Success : ", rsp)
+      } else {
+          console.log("Failed : ", rsp)
+      }
+  }
+)
+}
+
+    return (
+        <div>
+      <header className="App-header">
+      <SearchBar placeholder="search grouop activity member"/>
+        <p>
+          나는 홈페이지를 만들꺼야
+        </p>
+        <button onClick={callPaymentModule}>
+          이 버튼을 누르면 결제가 됨
+        </button>
+      </header>
+    </div>
+    )
+}
+
+export default PaymentPage
