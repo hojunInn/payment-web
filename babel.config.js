ef26989ce1d956935751695e7efbded2fc0fa302
hojunin <hjinn@smartauth.kr>
2021-01-23 22:50:04 +0900

    Docs 제거

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
-  return {
-    
+  return {   
       "presets": ["@babel/preset-react", "@babel/preset-env"],
       "plugins": ["@babel/plugin-syntax-jsx"]
-    
-  //   plugins: [
-  //     [
-  //       "module-resolver",
-  //       {
-  //         root: ["."],
-  //         extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.jsx', '.js', '.json'],
-  //         alias: {
-  //           '~': './src',
-  //           '@utils': './src/utils',
-  //           '@lib': './src/lib',
-  //           '@assets': './assets',
-  //           '@screens': './src/screens',
-  //           '@components': './src/components',
-  //           '@config': './config'
-  //         },
-  //       }
-  //     ]
-  //   ]
-  // };
-};
+}
